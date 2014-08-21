565096cabd5a4bdac4e8665379570acd9b86ae9f
Stefano Azzolini <lastguest@gmail.com>
2014-08-21 14:44:19 +0200

    Update Cache.php

--- classes/Cache.php
+++ classes/Cache.php
@@ -101,17 +101,4 @@ class Cache {
     }
 }
 
-interface CacheInterface  {
-    public function get($key);
-    public function set($key,$value,$expire=0);
-    public function delete($key);
-    public function exists($key);
-    public function flush();
-
-    public function inc($key,$value=1);
-    public function dec($key,$value=1);
-
-    public static function valid();
-}
-
 Cache::using('memory');
