be1c98d5fffba99406bf61e9640e37f1a5526652
Stefano Azzolini <lastguest@gmail.com>
2014-08-21 14:47:38 +0200

    Update Cache.php

--- classes/Cache.php
+++ classes/Cache.php
@@ -101,4 +101,28 @@ class Cache {
     }
 }
 
+/**
+ * CacheInterface
+ *
+ * Cache drivers common interface.
+ * 
+ * @package core
+ * @author stefano.azzolini@caffeinalab.com
+ * @version 1.0
+ * @copyright Caffeina srl - 2014 - http://caffeina.co
+ */
+
+interface CacheInterface  {
+    public function get($key);
+    public function set($key,$value,$expire=0);
+    public function delete($key);
+    public function exists($key);
+    public function flush();
+
+    public function inc($key,$value=1);
+    public function dec($key,$value=1);
+
+    public static function valid();
+}
+
 Cache::using('memory');
