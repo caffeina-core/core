e0c6d53f766fdad4c73bee75df495fcace625402
Stefano Azzolini <lastguest@gmail.com>
2014-08-29 15:38:28 +0200

    Included string rendererer accessor as a private function.

--- classes/String.php
+++ classes/String.php
@@ -27,34 +27,22 @@ class String {
 	 * @param mixed $v (default: null)	The array of values exposed in template.
 	 * @return string
 	 */
-	public static function render($t,$v=null){
-		$t = is_array($t)?'':$t;
-		if(empty($v)) return $t;
-		for ($e=explode('{{',$t),$r=$e[$i=0];
-			isset($e[++$i]) && $ss=explode('}}',$e[$i]);
-			$r.=(strpos($q=trim($ss[0]),'.')!==false?
-			self::_rh($q,$v):(isset($v[$q])?$v[$q]:''))
-			.end($ss));return $r;
-	}
-
-	/**
-	 * Fast string templating internal private accessory function.
-	 *
-	 * @author Stefano Azzolini <stefano.azzolini@caffeinalab.com>
-	 * @access private
-	 * @static
-	 * @param mixed &$f
-	 * @param mixed &$v
-	 * @return void
-	 */
-	private static function _rh(&$f,&$v) {
-			static $m;$m?:$m=array();
-			if(isset($m[$f])) return $m[$f];
-			for($t=strtok($f,'.'),$q=isset($v[$t])?$v:'';
-				isset($q[$t])&&$t!==false;
-				$q=isset($q[$t])?$q[$t]:'',$t=strtok('.')
-		); return $m[$f]=($t?'':$q);
-	}
+	 public static function render($t,$v=null){
+	   function _rh(&$f,&$v) {
+	     static $m;$m?:$m=array();
+	     if(isset($m[$f])) return $m[$f];
+	     for($t=strtok($f,'.'),$q=isset($v[$t])?$v:'';
+	       isset($q[$t])&&$t!==false;
+	       $q=isset($q[$t])?$q[$t]:'',$t=strtok('.')
+	      ); return $m[$f]=($t?'':$q);
+	   }
+	   $t = is_array($t)?'':$t; if(empty($v)) return $t;
+	   for ($e=explode('{{',$t),$r=$e[$i=0];
+	     isset($e[++$i]) && $ss=explode('}}',$e[$i]);
+	     $r.=(strpos($q=trim($ss[0]),'.')!==false?
+	     _rh($q,$v):(isset($v[$q])?$v[$q]:''))
+	     .end($ss));return $r;
+	 }
 
 
 } /* End of class */
