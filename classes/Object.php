89e034110296a5013c7da269929f0af65df76037
Stefano Azzolini <lastguest@gmail.com>
2014-09-17 09:44:14 +0200

    Update Object.php

--- classes/Object.php
+++ classes/Object.php
@@ -13,28 +13,54 @@
 
 class Object extends ArrayObject {
 
-    public function __construct($input=[],$deep=true){
-        if (is_array($data = (is_string($input)?json_decode($input,true):(array)$input))){
-            if ($deep) foreach ($data as $key => &$value) {
-                if (is_array($value) || is_a($value,'stdClass')){
-                    $value = new self($value);
+    /**
+     * An Object can wrap a StdClass, an array or an object from a JSON encoded string.
+     * 
+     * This class is useful for wrapping API responses and access their properties in
+     * an easy way.
+     * 
+     * @param mixed  $input The object/array/json_encoded object to wrap
+     * @param boolean $deep  Wrap also deep branches as Objects
+     */
+    public function __construct($input=[], $deep=true){
+        $data = is_string($input) ? json_decode($input,true) : (array)$input;
+        if (is_array($data)){
+            if ($deep) {
+                foreach ($data as $key => &$value) {
+                    if (is_array($value) || is_a($value,'stdClass')){
+                        $value = new self($value);
+                    }
                 }
             }
            parent::__construct($data, static::ARRAY_AS_PROPS);
         } else {
-            throw new InvalidArgumentException('Argument must be a string containing valid JSON, an array or an stdClass.');
+            throw new InvalidArgumentException(
+                'Argument must be a string containing valid JSON, an array or an stdClass.'
+            );
         }
     }
  
+    /**
+     * ArrayObject::offsetSet
+     */
     public function offsetSet($key, $value){   
-        is_array($value) ? parent::offsetSet($key, new static($value)) : parent::offsetSet($key, $value);
+        if ( is_array($value) )
+          parent::offsetSet($key, new static($value));
+        else
+          parent::offsetSet($key, $value);
     }
  
+    /**
+     * ArrayObject::offsetGet
+     */
     public function offsetGet($key){
         $raw = parent::offsetGet($key);
         return is_callable($raw) ? call_user_func($raw) : $raw;
     }
  
+    /**
+     * Emulate object methods
+     */
     public function __call($method, $args){
         $raw = parent::offsetGet($method);
         if (is_callable($raw)) {
@@ -43,16 +69,29 @@ class Object extends ArrayObject {
         }
     }
     
+    /**
+     * If casted as a string, return a JSON rappresentation of the wrapped payload
+     * @return string
+     */
     public function __toString(){
         return json_encode($this,JSON_NUMERIC_CHECK);
     }
     
-    public static function fetch(&$f,&$v) {
-      static $m;$m?:$m=[];
-      if(isset($m[$f])) return $m[$f];
-      $t = strtok($f,'.');$q=$v;
-      while(($q=isset($q[$t])?$q[$t]:'')&&($t=strtok('.')));
-      return $m[$f]=($t?'':$q);
+    /**
+     * Dot-Notation Array Path Resolver
+     * @param  string $path The dot-notation path
+     * @param  array $root The array to navigate
+     * @return mixed The pointed value 
+     */
+    public static function fetch($path, array & $root) {
+      $frag = strtok($path,'.');
+      $ptr = $root;
+      while (
+        ( $ptr = isset($ptr[$frag]) ? $ptr[$frag] : '' )
+        &&
+        ( $frag = strtok('.') )
+      );
+      return $frag ? '' : $ptr;
     }
  
 }
