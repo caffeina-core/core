931def0c66f19fd58652469bd57269fed2c52d73
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2014-09-23 01:50:30 +0200

    [TEST] Absolute path

--- tests/run_tests.php
+++ tests/run_tests.php
@@ -2,7 +2,7 @@
 include_once 'common.php';
 date_default_timezone_set('Europe/Rome');
 echo "Test started on ",date('Y-m-d H:i:s'),".\n\n";
-foreach (glob('test_*.php') as $testfile) include $testfile;
+foreach (glob(__DIR__.'/test_*.php') as $testfile) include $testfile;
 echo "\nTest ended on ",date('Y-m-d H:i:s'),".\n";
 echo "Results: ",$test_stats["PASS"]," tests passed, ",$test_stats["FAIL"]," tests failed.\n";
 echo "Done.\n";
