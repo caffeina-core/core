37ae756145b5fd5baccccf6097728bbd48739470
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2014-09-23 01:55:21 +0200

    [TEST] Fixed script

--- tests/run_tests.php
+++ tests/run_tests.php
@@ -6,3 +6,4 @@ foreach (glob(__DIR__.'/test_*.php') as $testfile) include $testfile;
 echo "\nTest ended on ",date('Y-m-d H:i:s'),".\n";
 echo "Results: ",$test_stats["PASS"]," tests passed, ",$test_stats["FAIL"]," tests failed.\n";
 echo "Done.\n";
+exit($test_stats["FAIL"]?1:0);
