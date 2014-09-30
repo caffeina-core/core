ce8814aebeaccc365a0e65f923d8215131c137c9
root <root@app1>
2014-09-30 13:19:58 +0200

    Langage draft

---
+++ classes/Language.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Language
+ *
+ * Localization tools.
+ *
+ * @package core
+ * @author stefano.azzolini@caffeinalab.com
+ * @version 1.0
+ * @copyright Caffeina srl - 2014 - http://caffeina.co
+ */
+
+
+ class Language extends Dictionary {
+    use Module;
+    protected static $current_lang = 'en';
+
+    public static function translate($text,$params=null){
+      $result = static::get(static::$current_lang.'.'.strtolower($text),$text);
+      return $params ? String::render($result) : $result; 
+    }
+
+    public static function using($lang){
+      static::$current_lang = strtolower(trim($lang));
+    }
+
+    public static function load($lang, $dictfile){
+      ob_start(); $lang = include($dictfile); ob_end_clean();
+      static::merge([$lang => $dictfile]);
+    }
+}
