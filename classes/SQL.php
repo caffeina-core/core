8c74550a4e6e8a85e3009461c74a50dea10d9fce
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2014-12-25 20:47:41 +0100

    SQL::run

--- classes/SQL.php
+++ classes/SQL.php
@@ -4,7 +4,7 @@
  * SQL
  *
  * SQL database access via PDO.
- * 
+ *
  * @package core
  * @author stefano.azzolini@caffeinalab.com
  * @version 1.0
@@ -18,7 +18,7 @@ class SQL {
   protected static $pdo        = null;
   protected static $queries    = [];
   protected static $last_exec_success = true;
-  
+
   public static function connect($dsn, $username=null, $password=null, $options=[]){
     static::$connection = [
       'dsn'        => $dsn,
@@ -43,13 +43,13 @@ class SQL {
           static::$connection['username'],
           static::$connection['password'],
           static::$connection['options']
-        
+
       );
       Event::triggerOnce('core.sql.connect');
     }
     return static::$pdo;
   }
-  
+
   public static function prepare($query){
     return isset(static::$queries[$query]) ? static::$queries[$query] : (static::$queries[$query] = static::connection()->prepare($query));
   }
@@ -80,34 +80,45 @@ class SQL {
   public static function value($query, $params=[], $column=0){
     $res = static::exec($query,$params);
     return $res ? $res->fetchColumn($column) : null;
-  }  
+  }
 
   public static function each($query, $params=[], callable $looper = null){
     // ($query,$looper) shorthand
     if ($looper===null && is_callable($params)) {$looper = $params; $params = [];}
     if( $res = static::exec($query,$params) ){
-      if(is_callable($looper)) 
+      if(is_callable($looper))
         while ($row = $res->fetchObject()) $looper($row);
       else
         return $res->fetchAll(PDO::FETCH_CLASS);
     }
-  }  
+  }
 
   public static function single($query, $params=[], callable $handler = null){
     // ($query,$handler) shorthand
     if ($handler===null && is_callable($params)) {$handler = $params; $params = [];}
     if( $res = static::exec($query,$params) ){
-        if (is_callable($handler)) 
+        if (is_callable($handler))
           $handler($res->fetchObject());
         else
           return $res->fetchObject();
     }
-  }  
+  }
 
+ public static function run($script){
+    $sql_path = Options::get('database.sql.path',APP_DIR.'/sql');
+    $sql_sep  = Options::get('database.sql.separator',';');
+    if (is_file($f = "$sql_path/$script.sql")){
+        $result = true;
+        foreach(explode($sql_sep,file_get_contents($f)) as $statement) {
+            $result = SQL::exec($statement);
+        }
+        return $result;
+    } else return false;
+  }
 
   public static function all($query, $params=[]){
     return static::each($query,$params);
-  }  
+  }
 
   public static function delete($table, $pks=null, $pk='id', $inclusive=true){
     if (null===$pks) {
@@ -128,7 +139,7 @@ class SQL {
     $q = "INSERT INTO `$table` (`".implode('`,`',$k)."`) VALUES (".implode(',',$pk).")";
     static::exec($q,$data);
     return static::$last_exec_success ? static::connection()->lastInsertId() : false;
-  }  
+  }
 
   public static function update($table, $data=[], $pk='id', $extra_where=''){
     if (false==is_array($data)) $data = (array)$data;
@@ -139,16 +150,16 @@ class SQL {
     $q = "UPDATE `$table` SET ".implode(', ',$k)." WHERE `$pk`=:$pk $extra_where";
     static::exec($q,$data);
     return static::$last_exec_success;
-  }  
+  }
 
   public static function insertOrUpdate($table, $data=[], $pk='id', $extra_where=''){
     if (false==is_array($data)) $data = (array)$data;
     if (empty($data[$pk])) return static::insert($table, $data);
     if( (string) static::value("SELECT `$pk` FROM `$table` WHERE `$pk`=? LIMIT 1", [$data[$pk]]) === (string) $data[$pk] ){
-        return static::update($table, $data, $pk, $extra_where); 
+        return static::update($table, $data, $pk, $extra_where);
     } else {
-        return static::insert($table, $data);         
+        return static::insert($table, $data);
     }
-  } 
-  
+  }
+
 }
