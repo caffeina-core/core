cc1fed5f78cab85c9b265d6b1f3aa4dd4f073262
Stefano Azzolini <lastguest@gmail.com>
2015-05-06 12:56:16 +0200

    Update Adapter.php

--- classes/View/Adapter.php
+++ classes/View/Adapter.php
@@ -14,8 +14,8 @@ namespace View;
 
 interface Adapter {
     public function __construct($path=null,$options=[]);
-    public function exists($path);
     public function render($template,$data=[]);
+    public static function exists($path);
     public static function addGlobal($key,$val);
     public static function addGlobals(array $defs);
 }
