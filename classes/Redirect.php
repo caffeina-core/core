5c9aa654e66f83fd0e637a09a681b39a6049260d
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2015-11-24 19:30:25 +0100

    [Redirect] new back method

--- classes/Redirect.php
+++ classes/Redirect.php
@@ -4,7 +4,7 @@
  * Redirect
  *
  * HTTP redirection commands.
- * 
+ *
  * @package core
  * @author stefano.azzolini@caffeinalab.com
  * @copyright Caffeina srl - 2015 - http://caffeina.it
@@ -14,15 +14,30 @@ class Redirect {
     use Module;
 
     public static function to($url){
-        Response::clean();
-        Response::header('Location',Filter::with('core.redirect',$url));
-        Response::send();
-        exit;
+    		if ($link = Filter::with('core.redirect',$url)) {
+	        Response::clean();
+	        Response::header('Location', $link);
+	        Response::send();
+	        exit;
+      	}
     }
 
-    public static function viaJavaScript($url,$parent=false){
+    public static function back(){
+        if ($link = Filter::with('core.redirect', (empty($_SERVER['HTTP_REFERER']) ? Request::get('redirect_uri',false) : $_SERVER['HTTP_REFERER']) )){
+	        Response::clean();
+	        Response::header('Location', $link);
+	        Response::send();
+	        exit;
+      	}
+    }
+
+    public static function viaJavaScript($url, $parent=false){
+    	if ($link = Filter::with('core.redirect', $url)){
         Response::type('text/html');
-        Response::add('<script>'.($parent?'parent.':'').'location.href="',addslashes(Filter::with('core.redirect',$url)),'"</script>');
+        Response::add('<script>'.($parent?'parent.':'').'location.href="',addslashes($link),'"</script>');
+        Response::send();
+	      exit;
+      }
     }
 
 }
