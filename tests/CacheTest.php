89e1bdc9dc0998bf70b029462e3ee290ed8e72d2
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2016-02-25 10:11:06 +0100

    Migrated old tests to PHPUnit

---
+++ tests/CacheTest.php
@@ -0,0 +1,32 @@
+<?php
+
+
+class CacheTest extends PHPUnit_Framework_TestCase {
+
+    public function __construct(){
+      Cache::using('memory');
+    }
+
+    public function testSetGet(){
+      Cache::set('test','ALPHA');
+      $this->assertEquals('ALPHA',Cache::get('test'));
+    }
+
+    public function testGetUnknown(){
+      $this->assertEquals('BETA',Cache::get('test2','BETA'));
+      $this->assertEquals('BETA',Cache::get('test2'));
+    }
+
+    public function testSetDefault(){
+      Cache::set('test','ALPHA');
+    }
+
+    public function testGetDefaultFromClosure(){
+      $this->assertEquals('SLOW_DATA :)',Cache::get('test3',function(){
+        return "SLOW_DATA :)";
+      }) );
+    }
+
+}
+
+
