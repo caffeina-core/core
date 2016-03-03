f49c09131e3f7312898e22dc67b0bcdc1b7c6df5
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2016-03-03 18:07:48 +0100

    [New] Hash::uuid

--- classes/Hash.php
+++ classes/Hash.php
@@ -13,7 +13,7 @@
 
 class Hash {
     use Module;
-   
+
     /**
      * Create ah hash for payload
      * @param  mixed $payload The payload string/object/array
@@ -59,14 +59,46 @@ class Hash {
     public static function can($algo){
         return in_array($algo,hash_algos());
     }
-    
+
     /**
      * Static magic for creating hashes with a specified algorithm.
-     * 
+     *
      * See [hash-algos](http://php.net/manual/it/function.hash-algos.php) for a list of algorithms
      */
     public static function __callStatic($method,$params){
         return self::make(current($params),$method);
     }
+
+    public static function uuid($type=4, $namespace='', $name=''){
+      switch($type){
+        case 3: if(preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+        '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/Si', $namespace) !== 1) return false;
+        $nhex = str_replace(array('-','{','}'), '', $namespace);
+        $nstr = ''; for($i = 0; $i < strlen($nhex); $i+=2)
+        $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+        $hash = md5($nstr . $name);
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+        substr($hash, 0, 8), substr($hash, 8, 4),
+        (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x3000,
+        (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+        substr($hash, 20, 12));
+        case 5: if(preg_match('/^\{?[0-9a-f]{8}\-?[0-9a-f]{4}\-?[0-9a-f]{4}\-?'.
+        '[0-9a-f]{4}\-?[0-9a-f]{12}\}?$/Si', $namespace) !== 1) return false;
+        $nhex = str_replace(array('-','{','}'), '', $namespace);
+        $nstr = ''; for($i = 0; $i < strlen($nhex); $i+=2)
+        $nstr .= chr(hexdec($nhex[$i].$nhex[$i+1]));
+        $hash = sha1($nstr . $name);
+        return sprintf('%08s-%04s-%04x-%04x-%12s',
+        substr($hash, 0, 8), substr($hash, 8, 4),
+        (hexdec(substr($hash, 12, 4)) & 0x0fff) | 0x5000,
+        (hexdec(substr($hash, 16, 4)) & 0x3fff) | 0x8000,
+        substr($hash, 20, 12));
+        default: case 4: return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000, mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff));
+      }
+    }
+
 
 }
---
+++ tests/HashTest.php
@@ -0,0 +1,32 @@
+<?php
+
+class HashTest extends PHPUnit_Framework_TestCase {
+
+  public function testAlgos(){
+    $this->assertNotEmpty($algos = Hash::methods());
+    $testAlgo = $algos[0];
+    $this->assertTrue(Hash::can($testAlgo));
+
+    $payload = [1,2,3];
+    $hash    = Hash::make($payload,$testAlgo);
+    $this->assertNotEmpty($hash);
+    $this->assertTrue(Hash::verify($payload,$hash,$testAlgo));
+  }
+
+  public function testUUID(){
+    $this->assertNotEquals(Hash::uuid(),Hash::uuid());
+    $namespace = Hash::uuid();
+
+    $this->assertFalse(Hash::uuid(3,'not-valid!','123'));
+    $this->assertEquals(Hash::uuid(3,$namespace,'123'),Hash::uuid(3,$namespace,'123'));
+    $this->assertNotEquals(Hash::uuid(3,$namespace,'123'),Hash::uuid(3,$namespace,'321'));
+    $this->assertNotEquals(Hash::uuid(3,Hash::uuid(),'123'),Hash::uuid(3,$namespace,'123'));
+
+    $this->assertFalse(Hash::uuid(5,'not-valid!','123'));
+    $this->assertEquals(Hash::uuid(5,$namespace,'123'),Hash::uuid(5,$namespace,'123'));
+    $this->assertNotEquals(Hash::uuid(5,$namespace,'123'),Hash::uuid(5,$namespace,'321'));
+    $this->assertNotEquals(Hash::uuid(5,Hash::uuid(),'123'),Hash::uuid(5,$namespace,'123'));
+  }
+
+}
+
