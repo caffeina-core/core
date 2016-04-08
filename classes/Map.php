0670c32e69b4cbcc2489f9327c2851a1a498e5e2
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2016-04-08 20:15:30 +0200

    Fixed Map static problem

--- classes/Map.php
+++ classes/Map.php
@@ -5,7 +5,7 @@
  *
  * The map class allow to handle a repository of key-values data.
  * Values are accessibles via a dot notation key path.
- * 
+ *
  * @package core
  * @author stefano.azzolini@caffeina.com
  * @copyright Caffeina srl - 2016 - http://caffeina.com
@@ -25,7 +25,7 @@ class Map implements JsonSerializable {
 
     /**
      * Get a value assigned to a key path from the map
-     * @param  string $key The key path of the value in dot notation 
+     * @param  string $key The key path of the value in dot notation
      * @param  mixed $default (optional) The default value. If is a callable it will executed and the return value will be used.
      * @return mixed The value of the key or the default (resolved) value if the key not existed.
      */
@@ -43,7 +43,7 @@ class Map implements JsonSerializable {
 
     /**
      * Set a value for a key path from map
-     * @param  string $key The key path of the value in dot notation 
+     * @param  string $key The key path of the value in dot notation
      * @param  mixed $value (optional) The value. If is a callable it will executed and the return value will be used.
      * @return mixed The value of the key or the default (resolved) value if the key not existed.
      */
@@ -58,7 +58,7 @@ class Map implements JsonSerializable {
 
     /**
      * Delete a value and the key path from map.
-     * @param  string $key The key path in dot notation 
+     * @param  string $key The key path in dot notation
      * @param  boolean $compact (optional) Compact map removing empty paths.
      */
     public function delete($key, $compact=true){
@@ -68,7 +68,7 @@ class Map implements JsonSerializable {
 
     /**
      * Check if a key path exists in map.
-     * @param  string $key The key path in dot notation 
+     * @param  string $key The key path in dot notation
      * @return boolean
      */
     public function exists($key){
@@ -85,7 +85,7 @@ class Map implements JsonSerializable {
     public function __construct($fields=null){
         $this->load($fields);
     }
-    
+
     /**
      * Load an associative array/object as the map source.
      * @param  string $fields The array to merge
@@ -95,7 +95,7 @@ class Map implements JsonSerializable {
     }
 
     /**
-     * Merge an associative array to the map. 
+     * Merge an associative array to the map.
      * @param  array   $array The array to merge
      * @param  boolean $merge_back If `true` merge the map over the $array, if `false` (default) the reverse.
      */
@@ -109,14 +109,14 @@ class Map implements JsonSerializable {
      * Compact map removing empty paths
      */
     protected function compact(){
-        function array_filter_rec($input, $callback = null) { 
-            foreach ($input as &$value) { 
-                if (is_array($value)) { 
-                    $value = array_filter_rec($value, $callback); 
-                } 
-            } 
-            return array_filter($input, $callback); 
-        } 
+        function array_filter_rec($input, $callback = null) {
+            foreach ($input as &$value) {
+                if (is_array($value)) {
+                    $value = array_filter_rec($value, $callback);
+                }
+            }
+            return array_filter($input, $callback);
+        }
 
         $this->fields = array_filter_rec($this->fields,function($a){ return $a !== null; });
     }
@@ -128,8 +128,8 @@ class Map implements JsonSerializable {
      * @param  callable  If passed this callback will be applied to the founded value.
      * @return mixed The founded value.
      */
-    protected static function & find($path, $create=false, callable $operation=null) {
-        $tok = strtok($path,'.');        
+    protected function & find($path, $create=false, callable $operation=null) {
+        $tok = strtok($path,'.');
         if($create){
             $value =& $this->fields;
         } else {
