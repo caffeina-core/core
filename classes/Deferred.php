07962bab2ed438ab93d760b2f01e9c17ec869467
Gabriele Diener <gabriele.diener@caffeina.com>
2016-04-13 16:01:08 +0200

    Add Deferred class

---
+++ classes/Deferred.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Deferred
+ *
+ * Run callback when script execution is stopped.
+ *
+ * @package core
+ * @author gabriele.diener@caffeina.it
+ * @copyright Caffeina srl - 2016 - http://caffeina.com
+ */
+
+class Deferred {
+
+	protected 	$callback,
+				$args;
+
+	public function __construct( callable $callback ) {
+		$this->args = array_slice( func_get_args(), 1 );
+		$this->callback = $callback;
+	}
+
+	public function setArgs() {
+		$this->args = func_get_args();
+	}
+
+	public function __destruct() {
+		call_user_func( $this->callback, $this->$args );
+	}
+}
