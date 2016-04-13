2c16e6c162272ac23c297db2e6efccf94137347f
Stefano Azzolini <stefano.azzolini@caffeinalab.com>
2016-04-13 19:08:34 +0200

    Fixed email typo and added some tests for Deferred class

--- classes/Deferred.php
+++ classes/Deferred.php
@@ -6,25 +6,26 @@
  * Run callback when script execution is stopped.
  *
  * @package core
- * @author gabriele.diener@caffeina.it
+ * @author gabriele.diener@caffeina.com
  * @copyright Caffeina srl - 2016 - http://caffeina.com
  */
 
 class Deferred {
 
-	protected 	$callback,
-				$args;
+  protected   $callback,
+              $args;
 
-	public function __construct( callable $callback ) {
-		$this->args = array_slice( func_get_args(), 1 );
-		$this->callback = $callback;
-	}
+  public function __construct( callable $callback ) {
+    $this->args     = array_slice( func_get_args(), 1 );
+    $this->callback = $callback;
+  }
 
-	public function setArgs() {
-		$this->args = func_get_args();
-	}
+  public function setArgs() {
+    $this->args = func_get_args();
+  }
 
-	public function __destruct() {
-		call_user_func( $this->callback, $this->args );
-	}
+  public function __destruct() {
+    call_user_func( $this->callback, $this->args );
+  }
+
 }
