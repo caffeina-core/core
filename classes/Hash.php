638416969dc11d8a7ffa29b79dc033dd9650d41d
Stefano Azzolini <lastguest@gmail.com>
2016-05-17 09:40:26 +0200

    Upd: Update Hash::murmur to latest source

--- classes/Hash.php
+++ classes/Hash.php
@@ -111,48 +111,39 @@ class Hash {
 	}
 
   public static function murmur($key, $seed = 0, $as_integer=false) {
-		$key = (string) $key;
-		$klen = strlen($key);
-		$h1 = $seed;
-		for ($i = 0, $bytes = $klen - ($remainder = $klen & 3); $i < $bytes;) {
-			$k1 = ((ord($key[$i]) & 0xff))
-			 | ((ord($key[++$i]) & 0xff) << 8)
-			 | ((ord($key[++$i]) & 0xff) << 16)
-			 | ((ord($key[++$i]) & 0xff) << 24);
-			++$i;
-			$k1 = (((($k1 & 0xffff) * 0xcc9e2d51)
-				 + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000)) * 0xcc9e2d51) & 0xffff) << 16)))
-			 & 0xffffffff;
-			$k1 = $k1 << 15 | ($k1 >= 0 ? $k1 >> 17 : (($k1 & 0x7fffffff) >> 17) | 0x4000);
-			$k1 = (((($k1 & 0xffff) * 0x1b873593) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000))
-				 * 0x1b873593) & 0xffff) << 16))) & 0xffffffff;
-			$h1 ^= $k1;
-			$h1 = $h1 << 13 | ($h1 >= 0 ? $h1 >> 19 : (($h1 & 0x7fffffff) >> 19) | 0x1000);
-			$h1b = (((($h1 & 0xffff) * 5) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000)) * 5)
-				 & 0xffff) << 16))) & 0xffffffff;
-			$h1 = ((($h1b & 0xffff) + 0x6b64) + ((((($h1b >= 0 ? $h1b >> 16 : (($h1b & 0x7fffffff) >> 16) | 0x8000))
-				 + 0xe654) & 0xffff) << 16));
-		}
-		$k1 = 0;
-		switch ($remainder) {
-		case 3:$k1 ^= (ord($key[$i + 2]) & 0xff) << 16;
-		case 2:$k1 ^= (ord($key[$i + 1]) & 0xff) << 8;
-		case 1:$k1 ^= (ord($key[$i]) & 0xff);
-			$k1 = ((($k1 & 0xffff) * 0xcc9e2d51) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000))
-				 * 0xcc9e2d51) & 0xffff) << 16)) & 0xffffffff;
-			$k1 = $k1 << 15 | ($k1 >= 0 ? $k1 >> 17 : (($k1 & 0x7fffffff) >> 17) | 0x4000);
-			$k1 = ((($k1 & 0xffff) * 0x1b873593) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000))
-				 * 0x1b873593) & 0xffff) << 16)) & 0xffffffff;
-			$h1 ^= $k1;
-		}
-		$h1 ^= $klen;
-		$h1 ^= ($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000);
-		$h1 = ((($h1 & 0xffff) * 0x85ebca6b) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000))
-			 * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
-		$h1 ^= ($h1 >= 0 ? $h1 >> 13 : (($h1 & 0x7fffffff) >> 13) | 0x40000);
-		$h1 = (((($h1 & 0xffff) * 0xc2b2ae35) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000))
-			 * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-		$h1 ^= ($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000);
+    $key  = array_values(unpack('C*',(string) $key));
+    $klen = count($key);
+    $h1   = (int)$seed;
+    for ($i=0,$bytes=$klen-($remainder=$klen&3) ; $i<$bytes ; ) {
+      $k1 = $key[$i]
+        | ($key[++$i] << 8)
+        | ($key[++$i] << 16)
+        | ($key[++$i] << 24);
+      ++$i;
+      $k1  = (((($k1 & 0xffff) * 0xcc9e2d51) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000)) * 0xcc9e2d51) & 0xffff) << 16))) & 0xffffffff;
+      $k1  = $k1 << 15 | ($k1 >= 0 ? $k1 >> 17 : (($k1 & 0x7fffffff) >> 17) | 0x4000);
+      $k1  = (((($k1 & 0xffff) * 0x1b873593) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000)) * 0x1b873593) & 0xffff) << 16))) & 0xffffffff;
+      $h1 ^= $k1;
+      $h1  = $h1 << 13 | ($h1 >= 0 ? $h1 >> 19 : (($h1 & 0x7fffffff) >> 19) | 0x1000);
+      $h1b = (((($h1 & 0xffff) * 5) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000)) * 5) & 0xffff) << 16))) & 0xffffffff;
+      $h1  = ((($h1b & 0xffff) + 0x6b64) + ((((($h1b >= 0 ? $h1b >> 16 : (($h1b & 0x7fffffff) >> 16) | 0x8000)) + 0xe654) & 0xffff) << 16));
+    }
+    $k1 = 0;
+    switch ($remainder) {
+      case 3: $k1 ^= $key[$i + 2] << 16;
+      case 2: $k1 ^= $key[$i + 1] << 8;
+      case 1: $k1 ^= $key[$i];
+      $k1  = ((($k1 & 0xffff) * 0xcc9e2d51) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000)) * 0xcc9e2d51) & 0xffff) << 16)) & 0xffffffff;
+      $k1  = $k1 << 15 | ($k1 >= 0 ? $k1 >> 17 : (($k1 & 0x7fffffff) >> 17) | 0x4000);
+      $k1  = ((($k1 & 0xffff) * 0x1b873593) + ((((($k1 >= 0 ? $k1 >> 16 : (($k1 & 0x7fffffff) >> 16) | 0x8000)) * 0x1b873593) & 0xffff) << 16)) & 0xffffffff;
+      $h1 ^= $k1;
+    }
+    $h1 ^= $klen;
+    $h1 ^= ($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000);
+    $h1  = ((($h1 & 0xffff) * 0x85ebca6b) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000)) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+    $h1 ^= ($h1 >= 0 ? $h1 >> 13 : (($h1 & 0x7fffffff) >> 13) | 0x40000);
+    $h1  = (((($h1 & 0xffff) * 0xc2b2ae35) + ((((($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000)) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+    $h1 ^= ($h1 >= 0 ? $h1 >> 16 : (($h1 & 0x7fffffff) >> 16) | 0x8000);
 
 		return $as_integer ? $h1 : base_convert($h1 ,10, 32);
 	}
